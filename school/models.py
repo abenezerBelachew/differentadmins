a94db7c95d6bc28d1a7f23f2b2c57c09df9c80a5
Abenezer Belachew <belachew@ualberta.ca>
2021-07-16 15:49:53 -0600

    Style Changes

--- school/models.py
+++ school/models.py
@@ -1,18 +1,19 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
 
 
 User = settings.AUTH_USER_MODEL
 
 
 class Subject(models.Model):
+    """
+    Name and description of the subject.
+    """
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=120, blank=True)
 
     def __str__(self):
         return self.name
-    
-
 
 class Grade(models.Model):
     GRADE_CHOICES = (
@@ -27,17 +28,13 @@ class Grade(models.Model):
     subject = models.ForeignKey(to=Subject, on_delete=models.CASCADE, related_name="grades")
     grade = models.CharField(max_length=4, choices=GRADE_CHOICES, default='U')
 
-
     def __str__(self):
         return self.student.name
     
-
-
 class Advice(models.Model):
     student = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name="advices")
     advice = models.TextField(max_length=240)
 
-
     def __str__(self):
-        return f"Advice for {self.student.name}"
+        return self.student.name
     
